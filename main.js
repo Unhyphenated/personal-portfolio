5c2ce377b8f6e4101ea118cdae4958125617c7e1
Unhyphenated <julian.jong74@gmail.com>
2023-03-02 20:35:34 +0800

    Make pop-up menu disappear.

--- main.js
+++ main.js
@@ -13,4 +13,13 @@ if (navClose) {
     navClose.addEventListener("click", () => {
         navMenu.classList.remove("show-menu")
     })
+} 
+
+const navLink = document.querySelectorAll(".nav__link")
+
+const linkAction = () => {
+    const navMenu = document.getElementById("nav-menu")
+    navMenu.classList.remove("show-menu")
 }
+
+navLink.forEach(n => n.addEventListener("click", linkAction))
